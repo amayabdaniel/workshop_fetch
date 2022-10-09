5fcae7d239091164242dc6c25041ff9bda64769d
Daniel Amaya B <daniel.amaya.buitrago@outlook.com>
2022-10-09 14:41:39 -0500

    [UPDATE] changes in ui

--- src/index.js
+++ src/index.js
@@ -2,29 +2,31 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
-const url = 'https://platzi-avo.vercel.app/api/avo';
+const baseUrl = 'https://platzi-avo.vercel.app';
+
 //web api
 //connexión a la web api
 window
-    .fetch(url)
+    .fetch(`${baseUrl}/api/avo`)
     .then((respuesta) => respuesta.json())
     .then(responseJson => {
         const todosLosItems = [];
         responseJson.data.forEach(item => {
             //imagen
-            const img = document.createElement("img");
+            const imagen = document.createElement("img");
+            imagen.src = `${baseUrl}${item.image}`;
 
             //title
             const title = document.createElement("h2");
+            title.textContent = item.name;
 
             //precio
             const price = document.createElement("div");
+            price.textContent = item.price;
 
 
             const container = document.createElement("div");
-            container.append(img, title, price);
-
-            document.body.append(container);
+            container.append(imagen, title, price);
 
             todosLosItems.push(container);
         });
